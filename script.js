555fd181618f79263ff9890e1e64a948a6c92337
Matt Hildebrand <hildebrand.matt@gmail.com>
2021-07-28 16:05:20 -0700

    Flip control added

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function flip(event){
     var element = event.currentTarget;
     if (element.classList.contains("card-slip")) {
-        console.log(element.lastElementChild);
+
+        // Check for other flipped cards
+        flipCheck(element);
+
+        // Flipping the current card,
         if (element.firstElementChild.style.transform == "rotateY(180deg)") {
             element.firstElementChild.style.transform = "rotateY(0deg)";
         } else {
@@ -11,6 +15,32 @@ function flip(event){
             element.lastElementChild.style.transform = "rotateY(180deg)";
         } else {
             element.lastElementChild.style.transform = "rotateY(0deg)";
+        }
+        
+        // Add "flipped" to class names
+        element.classList.add("flipped");
+    }
+}
+
+function flipCheck(element) {
+
+    // Create sibling array
+    var cardArray = [];
+    let sibling = element.parentNode.firstElementChild;
+    while (sibling) {
+        cardArray.push(sibling);
+        sibling = sibling.nextElementSibling;
+    }
+
+    // Check array for flipped cards
+    cardArray.forEach(unflip);
+
+    // Function for unflipping cards
+    function unflip(card) {
+        if (card.classList.contains("flipped") && card != element) {
+            card.firstElementChild.style.transform = "rotateY(0deg)";
+            card.lastElementChild.style.transform = "rotateY(180deg)";
+            card.classList.remove("flipped")
         }
     }
 }
